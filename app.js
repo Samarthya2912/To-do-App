e9f793e64853f1727ce93e90b06fa7136de6c15a
Samarthya2912 <samarthya55@gmail.com>
2021-07-30 22:30:25 +0530

    project init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render('index');
+})
+
+app.listen(3000, ()=>{
+    console.log("Server started on port 3000.");
+})
